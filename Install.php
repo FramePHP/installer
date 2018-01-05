6812f302aeea7427ddb9e107e79a5bf56b62089e
HilkiahMakemo <hilkiah.makemo@skywest.com>
2018-01-04 19:19:31 -0700

    Update Install.php

--- Install.php
+++ Install.php
@@ -1,16 +1,18 @@
 <?php
 /**
-* 
+*
 */
-class Install
+define('BASE_DIR', __DIR__.'/');
+define('APP_PATH', __DIR__.'/app/');
+define('SYS_PATH', __DIR__.'/sys/');
+
+var $DeleteFiles = function($folder)
 {
-	
-	public function postInstall()
-	{
-		dump('HI - post install here!');
-	}
-	public function postUpdate()
-	{
-		dump('HI - post update here!!');
-	}
+  foreach (glob($folder.'/*', GLOB_NOSORT) as $file) {
+    if(stripos($file,'framework') !== false) return;
+    if(is_dir($file)) return $DeleteFiles($file);
+    if(stripos($file,'module-aop')) rename($file, APP_PATH.basename($file));
+    if(stripos($file,'module-sys')) rename($file, SYS_PATH.basename($file));
+  }
 }
+$DeleteFiles('vendor/frame-php');
